90cdc4adbbce76e1187562ecf0971710a85ea2f0
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-27 16:02:04 -0600

    Update file.go
    Delete

--- bigquery/file.go
+++ bigquery/file.go
@@ -1,46 +1,77 @@
-// Copyright 2016 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+Delete
 
-package bigquery
 
-import (
-	"io"
 
-	bq "google.golang.org/api/bigquery/v2"
-)
 
-// A ReaderSource is a source for a load operation that gets
-// data from an io.Reader.
-//
-// When a ReaderSource is part of a LoadConfig obtained via Job.Config,
-// its internal io.Reader will be nil, so it cannot be used for a
-// subsequent load operation.
-type ReaderSource struct {
-	r io.Reader
-	FileConfig
-}
 
-// NewReaderSource creates a ReaderSource from an io.Reader. You may
-// optionally configure properties on the ReaderSource that describe the
-// data being read, before passing it to Table.LoaderFrom.
-func NewReaderSource(r io.Reader) *ReaderSource {
-	return &ReaderSource{r: r}
-}
 
-func (r *ReaderSource) populateLoadConfig(lc *bq.JobConfigurationLoad) io.Reader {
-	r.FileConfig.populateLoadConfig(lc)
-	return r.r
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 // FileConfig contains configuration options that pertain to files, typically
