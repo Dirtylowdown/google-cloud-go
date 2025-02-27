4b8b6b5bcbda56bd7f0b4bc6baaddf1ff1623f8c
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-27 16:12:23 -0600

    Update inserter.go
    Delete

--- bigquery/inserter.go
+++ bigquery/inserter.go
@@ -1,38 +1,4 @@
-// Copyright 2015 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package bigquery
-
-import (
-	"context"
-	"errors"
-	"fmt"
-	"reflect"
-
-	"cloud.google.com/go/internal/trace"
-	bq "google.golang.org/api/bigquery/v2"
-)
-
-// NoDedupeID indicates a streaming insert row wants to opt out of best-effort
-// deduplication.
-// It is EXPERIMENTAL and subject to change or removal without notice.
-const NoDedupeID = "NoDedupeID"
-
-// An Inserter does streaming inserts into a BigQuery table.
-// It is safe for concurrent use.
-type Inserter struct {
-	t *Table
+Delete
 
 	// SkipInvalidRows causes rows containing invalid data to be silently
 	// ignored. The default value is false, which causes the entire request to
