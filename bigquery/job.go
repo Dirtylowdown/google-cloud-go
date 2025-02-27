4194d6ab0224a12d585a908a051c84349ef7a07a
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-27 16:15:03 -0600

    Update job.go
    Delete

--- bigquery/job.go
+++ bigquery/job.go
@@ -1,43 +1,75 @@
-// Copyright 2015 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+Delete 
 
-package bigquery
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	"time"
 
-	"cloud.google.com/go/internal"
-	"cloud.google.com/go/internal/trace"
-	gax "github.com/googleapis/gax-go/v2"
-	bq "google.golang.org/api/bigquery/v2"
-	"google.golang.org/api/googleapi"
-	"google.golang.org/api/iterator"
-)
 
-// A Job represents an operation which has been submitted to BigQuery for processing.
-type Job struct {
-	c          *Client
-	projectID  string
-	jobID      string
-	location   string
-	email      string
-	config     *bq.JobConfiguration
-	lastStatus *JobStatus
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // JobFromID creates a Job which refers to an existing BigQuery job. The job
 // need not have been created by this package. For example, the job may have
