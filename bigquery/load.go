82bcd7fdc4f898083a34ef22b63c40d15e9771ae
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-27 16:16:12 -0600

    Update load.go
    Delete

--- bigquery/load.go
+++ bigquery/load.go
@@ -1,40 +1,73 @@
-// Copyright 2016 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+delete
 
-package bigquery
 
-import (
-	"context"
-	"io"
-	"time"
 
-	"cloud.google.com/go/internal/trace"
-	bq "google.golang.org/api/bigquery/v2"
-	"google.golang.org/api/googleapi"
-)
 
-// LoadConfig holds the configuration for a load job.
-type LoadConfig struct {
-	// Src is the source from which data will be loaded.
-	Src LoadSource
 
-	// Dst is the table into which the data will be loaded.
-	Dst *Table
 
-	// CreateDisposition specifies the circumstances under which the destination table will be created.
-	// The default is CreateIfNeeded.
-	CreateDisposition TableCreateDisposition
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	// WriteDisposition specifies how existing data in the destination table is treated.
 	// The default is WriteAppend.
